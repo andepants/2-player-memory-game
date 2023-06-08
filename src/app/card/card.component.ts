cbf3d523e1519afc2f8c6de3e2edc2d555d3b9ec
Andrew Heim <andrewsheim@gmail.com>
2023-06-08 08:44:42 -0600

    implemented scoring

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -17,6 +17,10 @@ export class CardComponent {
   @Input() resetGuesses!: Function;
   @Input() turn!: boolean;
   @Output() turnChange = new EventEmitter<boolean>();
+  @Input() player1Score!: number;
+  @Input() player2Score!: number;
+  @Output() player1ScoreChange = new EventEmitter<number>();
+  @Output() player2ScoreChange = new EventEmitter<number>();
 
   constructor(private chosenCardService: ChosenCardService) {
     this.chosenCardState = this.chosenCardService.getState();
@@ -30,33 +34,35 @@ export class CardComponent {
   }
 
   updateTurnState(turn : boolean): void {
-    console.log(this.chosenCardService.getTurn(), 'this.chosenCardService.getTurn()');
     this.turn = this.chosenCardService.getTurn();
     this.chosenCardService.setTurn(!turn);
     this.turn = this.chosenCardService.getTurn();
     this.turnChange.emit(!turn)
-    console.log('this.turn', this.turn);
   }
 
   handleClick(): any {
-    console.log('shuffledCards', this.shuffledCards);
     this.chosenCardState = this.chosenCardService.getState();
     if (this.card.show === 'chosen') { // already chosen
       return;
     }
     this.card.show = 'chosen';
     if (this.chosenCardState[this.card.value]) { // found match
+      if (this.turn) {
+        this.player1Score++;
+        this.player1ScoreChange.emit(this.player1Score);
+      } else {
+        this.player2Score++;
+        this.player2ScoreChange.emit(this.player1Score);
+      }
       this.flipAllMatches(this.shuffledCards);
     }
     this.updateChosenCardState(this.card)
     if (Object.keys(this.chosenCardState).length >= 2) { // no match - 2 cards picked
-      console.log('chose 2+ cards');
       setTimeout(() => {
         this.resetGuesses(this.shuffledCards);
         this.chosenCardService.setState({});
         this.updateTurnState(this.turn);
       }, 1000);
     }
-    console.log(this.chosenCardService.getState(), 'chosenCardService.getState()');
   }
 }
